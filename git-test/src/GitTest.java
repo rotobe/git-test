c30f171fedfaeb8dc9f4b3ed8e91039f1fae0452
rotobe <1004008507@qq.com>
2020-04-18 09:39:53 +0800

    修改git-test

--- git-test/src/GitTest.java
+++ git-test/src/GitTest.java
@@ -7,4 +7,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好");
     }
+
+    public void test(){
+        System.out.println("hello");
+    }
 }
